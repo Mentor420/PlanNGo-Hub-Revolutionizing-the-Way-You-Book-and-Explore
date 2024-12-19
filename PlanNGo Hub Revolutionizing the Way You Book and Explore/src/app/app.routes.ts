785771da1f542a061a29b664887315de83312514
rankesh2004 <rangesh.csr.2004@gmail.com>
2024-12-19 21:51:46 +0530

    Flight commit

--- PlanNGo Hub Revolutionizing the Way You Book and Explore/src/app/app.routes.ts
+++ PlanNGo Hub Revolutionizing the Way You Book and Explore/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { FlightComponent } from './flight/components/flight/flight.component';
+import { CancelBookingComponent } from './flight/components/cancel-booking/cancel-booking.component';
+import { BookingHistoryComponent } from './flight/components/booking-history/booking-history.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"flights",
+        component:FlightComponent
+    },
+    {
+        path:"cancel-booking",
+        component:CancelBookingComponent
+    },
+    {
+        path:"booking-history",
+        component:BookingHistoryComponent
+    }
+];
