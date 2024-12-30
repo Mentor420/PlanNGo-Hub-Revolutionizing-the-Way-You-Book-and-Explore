f1482573f2340ed25a0ac0f128e193812533041e
RIDAMSINHA <136902050+RIDAMSINHA@users.noreply.github.com>
2024-12-30 12:48:58 +0530

    commenting this component for now

--- PlanNGo Hub Revolutionizing the Way You Book and Explore/src/app/hotels/components/page/page.component.ts
+++ PlanNGo Hub Revolutionizing the Way You Book and Explore/src/app/hotels/components/page/page.component.ts
@@ -1,18 +1,18 @@
-import { Component } from '@angular/core';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
-import { faUtensils } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+// import { Component } from '@angular/core';
+// import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+// import { faUtensils } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
-@Component({
-  selector: 'app-page',
-  standalone: true,
-  imports: [FontAwesomeModule],
-  templateUrl: './page.component.html',
-  styleUrl: './page.component.css'
+// @Component({
+//   selector: 'app-page',
+//   standalone: true,
+//   imports: [FontAwesomeModule],
+//   templateUrl: './page.component.html',
+//   styleUrl: './page.component.css'
   
 
-})
-export class PageComponent {
-  faCoffee = faCoffee;
-  faUtensils = faUtensils;
-}
+// })
+// export class PageComponent {
+//   faCoffee = faCoffee;
+//   faUtensils = faUtensils;
+// }
