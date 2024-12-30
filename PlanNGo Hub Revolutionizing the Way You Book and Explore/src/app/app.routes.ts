af09e78182165a340b8862d167dcf919974dfafc
RIDAMSINHA <136902050+RIDAMSINHA@users.noreply.github.com>
2024-12-30 23:59:57 +0530

    updating routes

--- PlanNGo Hub Revolutionizing the Way You Book and Explore/src/app/app.routes.ts
+++ PlanNGo Hub Revolutionizing the Way You Book and Explore/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Route } from '@angular/router';
 import { PlanNGoComponent } from './hotels/components/planngo/planngo.component';
 import { SectionComponent } from './hotels/components/section/section.component';
 import { SearchResultsComponent } from './hotels/components/search-results/search-results.component';
-// import { PageComponent } from './hotels/components/page/page.component';
+import { PageComponent } from './hotels/components/page/page.component';
 import { AboutComponent } from './hotels/components/about/about.component';
 import { ContactComponent } from './hotels/components/contact/contact.component';
 export const routes: Route[] = [
@@ -11,5 +11,5 @@ export const routes: Route[] = [
   { path: 'search-results', component: SearchResultsComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
-  // { path: 'page', component: PageComponent }, 
+  { path: 'page', component: PageComponent }, 
 ];
