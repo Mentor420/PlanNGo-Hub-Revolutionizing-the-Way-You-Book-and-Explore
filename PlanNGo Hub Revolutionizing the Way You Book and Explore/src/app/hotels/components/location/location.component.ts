5080f8954ee2f4556249af6da6951f9ffd52eb97
RIDAMSINHA <136902050+RIDAMSINHA@users.noreply.github.com>
2025-01-01 20:37:10 +0530

    Update location.component.ts

--- PlanNGo Hub Revolutionizing the Way You Book and Explore/src/app/hotels/components/location/location.component.ts
+++ PlanNGo Hub Revolutionizing the Way You Book and Explore/src/app/hotels/components/location/location.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import data from '../../models/db.json'; 
+import { Location } from '@angular/common';
+import { CommonModule } from '@angular/common';
+interface Hotel {
+  id: string;
+  city: string;
+  name: string;
+  pricePerNight: number;
+  roomsAvailable: number;
+  amenities: string[];
+}
 
 @Component({
   selector: 'app-location',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './location.component.html',
-  styleUrl: './location.component.css'
+  styleUrls: ['./location.component.css']
 })
-export class LocationComponent {
+export class LocationComponent implements OnInit {
+  city: string = '';
+  hotels: Hotel[] = []; // Ensures `hotels` is always an array
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      this.city = params['city'] || '';
+      this.loadHotels();
+    });
+  }
+
+  loadHotels(): void {
+    const allHotels: Hotel[] = data.hotels || []; // Safely access data
+    this.hotels = allHotels.filter(hotel =>
+      hotel.city.toLowerCase() === this.city.toLowerCase()
+    );
+  }
 }
