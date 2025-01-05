8cc0dc4e7c124818766c97cd0710bda3a3a13326
lx-rahul <rahulali05022003@.com>
2025-01-05 15:18:59 +0530

    added new pages

--- PlanNGo Hub Revolutionizing the Way You Book and Explore/src/app/app.routes.ts
+++ PlanNGo Hub Revolutionizing the Way You Book and Explore/src/app/app.routes.ts
@@ -5,6 +5,8 @@ import { PageComponent } from './hotels/components/page/page.component';
 import { AboutComponent } from './hotels/components/about/about.component';
 import { ContactComponent } from './hotels/components/contact/contact.component';
 import { LocationComponent } from './hotels/components/location/location.component';
+import { BookingFormComponent } from './hotels/components/booking-form/booking-form.component';
+
 
 export const routes: Route[] = [
   { path: '', component: SectionComponent },  
@@ -13,5 +15,6 @@ export const routes: Route[] = [
   { path: 'contact', component: ContactComponent },
   { path: 'page', component: PageComponent }, 
   { path: 'location', component: LocationComponent }, 
+  { path: 'booking-form', component: BookingFormComponent},
 
 ];
